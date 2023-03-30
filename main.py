d6b7fb02fcb9fd6b5d97dc3d6e87fd1922a09349
rohan37-pro <rohan2002maji@gmail.com>
2023-03-30 11:49:07 +0530

    jumble word solved

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from Jumble import jumble
 
 words_list = jumble.getword_list()
@@ -10,5 +10,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/solve", methods=["POST"])
+def solve():
+    if request.method=="POST":
+        jumble_word = request.get_data().decode()
+        solved_data = jumble.solve(jumble_word, words_list)
+        return solved_data
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
